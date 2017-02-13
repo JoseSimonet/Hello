64cba2f690f99e80225d2f8d58fcf07891f66274
Jose María <Jose Mara@JS>
2017-02-13 20:12:16 +0000

    Commit

---
+++ Hellow/src/Años.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Años {
+	
+		private static Scanner teclado;
+		
+		public static void main(String[] args) {
+			//Esto es una modificación.
+			teclado = new Scanner(System.in);
+			int AñoNac,AñoAct;
+			System.out.println("Por favor introduzca su año de nacimiento: ");
+			AñoNac = teclado.nextInt();
+			System.out.println("Por favor introduzca el año actual: ");
+			AñoAct = teclado.nextInt();
+			double edad;
+		
+			edad = (AñoAct-AñoNac);
+			
+			System.out.println("Su edad es: " +edad);
+		}
+}
