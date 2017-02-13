8efc1c1dce436d81d7bd623f89614d1d2680df52
Jose María <Jose Mara@JS>
2017-02-13 20:17:35 +0000

    coooomit

--- Hellow/src/Años.java
+++ Hellow/src/Años.java
@@ -6,7 +6,8 @@ public class Años {
 		private static Scanner teclado;
 		
 		public static void main(String[] args) {
-			//Esto es una modificación.
+			//Esto es una modificación..
+			//Edicion.
 			teclado = new Scanner(System.in);
 			int AñoNac,AñoAct;
 			System.out.println("Por favor introduzca su año de nacimiento: ");
